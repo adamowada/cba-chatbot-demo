4102559d121fa33f9a6cbdf14f9b375af7fda02f
Adam Owada <adamowada@gmail.com>
2024-08-14 17:29:50 -0700

    dynamic vh with useEffect

--- app/examples/basic-chat/page.tsx
+++ app/examples/basic-chat/page.tsx
@@ -1,18 +1,25 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./page.module.css"; // use simple styles for demonstration purposes
 import Chat from "../../components/chat";
 
 const Home = () => {
-  function setVhUnit() {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
-  }
+  useEffect(() => {
+    function setVhUnit() {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    }
 
-  // Call the function initially and on resize
-  setVhUnit();
-  window.addEventListener("resize", setVhUnit);
+    // Call the function initially and on resize
+    setVhUnit();
+    window.addEventListener("resize", setVhUnit);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", setVhUnit);
+    };
+  }, []); // The empty dependency array ensures this runs only once on mount
 
   return (
     <main className={styles.main}>
